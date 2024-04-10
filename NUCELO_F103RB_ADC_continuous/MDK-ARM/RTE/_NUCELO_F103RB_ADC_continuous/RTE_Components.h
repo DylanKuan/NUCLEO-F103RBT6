a188c3a39c8bc1894f84d29dec445df68762c50d
DylanKuan <kuancy1999@gmail.com>
2024-04-10 16:52:45 +0800

    Upload ADC projects and reference documents for board.

---
+++ NUCELO_F103RB_ADC_continuous/MDK-ARM/RTE/_NUCELO_F103RB_ADC_continuous/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'NUCELO_F103RB_ADC_continuous' 
+ * Target:  'NUCELO_F103RB_ADC_continuous' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
